e4aba905b2976340daa2ea02923e38d1578ef743
travis joe <weelgod@gmail.com>
2018-07-04 06:49:45 +0800

    init

---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Log Me In',
+      home: Scaffold(
+        body: Text('Show a form here'),
+      ),
+    );
+  }
+}
