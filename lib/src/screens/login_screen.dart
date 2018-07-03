cae9c8a33d356fa5365eed059f77d208fbd5005b
travis joe <weelgod@gmail.com>
2018-07-04 07:58:46 +0800

    109

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+          child: Column(
+        children: <Widget>[emailField(), passwordField(), submitButton()],
+      )),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return TextFormField();
+  }
+}
