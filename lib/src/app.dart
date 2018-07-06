b402fe4ee415dd5cd0f9461e57530e60044ac3b2
travis joe <weelgod@gmail.com>
2018-07-06 08:05:30 +0800

    153. single global instance finished

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 // import 'screens/login_screen_state.dart';
 import 'screens/login_screen_bloc.dart';
+import 'blocs/provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
+    return Provider(
+        child: MaterialApp(
       title: 'Log Me In',
       home: Scaffold(
         body: LoginScreenBloc(),
       ),
-    );
+    ));
   }
 }
